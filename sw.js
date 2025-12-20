83711623944c02d7b3fd6c6872e7cf273a03d156
Tok-DoT <tagochan8@gmail.com>
2025-12-20 17:49:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,32 @@
-const CACHE_NAME = "softtennis-cache-v1";
+const CACHE_NAME = "softtennis-cache-v2";
 const ASSETS = [
+  "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
 ];
 
-// インストール時にキャッシュ
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// リクエスト時にキャッシュ優先
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
